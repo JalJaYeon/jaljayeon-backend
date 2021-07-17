55972101e563c4d08f0b2fd17f33fe23a0c8558a
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-18 02:05:39 +0900

    feat(user): Sleep 을 어드민 user 에 inline으로 추가

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from apps.sleep.models import Sleep
 from apps.user.models import User
 
-admin.site.register(User)
+
+class SleepInline(admin.StackedInline):
+    model = Sleep
+
+
+@admin.register(User)
+class User(admin.ModelAdmin):
+    inlines = [SleepInline]
