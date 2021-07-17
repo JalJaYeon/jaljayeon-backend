0db121f2ad467ed251c254f9acdc16870ba66fd6
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-18 04:48:15 +0900

    feat(sleep): admin 의 list 에서 필드들이 수정 가능하도록 변경

--- apps/sleep/admin.py
+++ apps/sleep/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from apps.sleep.models import Sleep
 
-admin.site.register(Sleep)
+
+@admin.register(Sleep)
+class Sleep(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'slept_date', 'slept_time',
+                    'is_enough_sleep', 'used_phone_30_mins_before_sleep',
+                    'tiredness_level')
+    list_editable = list_display[1:]
