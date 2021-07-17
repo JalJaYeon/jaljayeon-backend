492d59774adf2b3a9dca52f1ccd57278566c74ce
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-18 00:43:50 +0900

    fix(common): 테스트 용 유틸 함수 버그 수정

--- apps/common/test.py
+++ apps/common/test.py
@@ -4,17 +4,16 @@ from apps.user.models import User
 
 
 def create_sample_user_and_get_token(api_client, username) -> typing.List:
-    user: User = User.objects.create_user(
-        username=username,
-        name="홍길동",
-        weight_kg=0,
-        average_sleep_time=time(00, 00),
-        bedtime_starts_at=time(00, 00),
-    )
+    user: User = User.objects.create_user(username=username,
+                                          name="홍길동",
+                                          weight_kg=0,
+                                          average_sleep_time=time(00, 00),
+                                          bedtime_starts_at=time(00, 00),
+                                          password='thePas123Q')
     response = api_client.post(
         '/api/token',
         {
-            'phone_number': username,
+            'username': username,
             'password': 'thePas123Q',
         },
     )
