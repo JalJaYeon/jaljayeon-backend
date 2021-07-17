80a10cb41e7639295c474aaf28f26f951daf855c
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-18 06:13:28 +0900

    fix(sleep): ai_advice의 mock response 오타 수정

--- apps/sleep/serializers.py
+++ apps/sleep/serializers.py
@@ -17,7 +17,7 @@ class SleepSerializer(serializers.ModelSerializer):
     ai_advice: str = serializers.SerializerMethodField()
 
     def get_ai_advice(self, obj: Sleep):
-        if obj.tiredness_level < 3:
+        if obj.tiredness_level > 3:
             return "- 수면 시간을 7시간으로 늘리세요\n- 취침 전 휴대폰 사용은 좋지 않아요"
         return "좋은 수면을 하셨습니다. 앞으로 유지해주세요"
 
