1bb93d8d4f65230fdaeaf959a0b7d48d7c887083
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-18 00:53:05 +0900

    feat(sleep): SleepView 추가

--- apps/sleep/views.py
+++ apps/sleep/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from apps.sleep.models import Sleep
+from rest_framework import mixins, viewsets
+from apps.sleep.permissions import IsOwner
+from apps.sleep.serializers import SleepSerializer
 
-# Create your views here.
+
+class SleepView(mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
+    serializer_class = SleepSerializer
+    permission_classes = (IsOwner, )
+
+    def perform_create(self, serializer: SleepSerializer):
+        serializer.is_valid(raise_exception=True)
+        serializer.save(owner=self.request.user)
+
+    def get_queryset(self):
+        if self.action == 'list':
+            return Sleep.objects.filter(owner=self.request.user)
+        return Sleep.objects.all()
