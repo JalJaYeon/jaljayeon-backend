7cf6ffa005178a47cccc981d02b049af1dc507d1
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-17 21:55:59 +0900

    feat(user): UserView 추가
    다음의 역할을 하는 UserView 추가
    - 회원가입
    - 요청자의 프로필 조회

---
+++ apps/user/utils.py
@@ -0,0 +1,3 @@
+def format_time(time_str: str):
+    """change time format from %H:%M:%S to %H:%M"""
+    return time_str[0:5]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from datetime import time
+from django.http.request import HttpRequest
+from rest_framework import viewsets, status, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from apps.user.models import User
+from apps.user.utils import format_time
+from apps.user.serializers import UserSerializer
 
-# Create your views here.
+
+class UserView(viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'register':
+            return (permissions.AllowAny(), )
+        return (permissions.IsAuthenticated(), )
+
+    def list(self, request: HttpRequest):  # retrieve requested user's profile
+        user: User = request.user
+        serializer = self.get_serializer(user)
+        return Response(serializer.data)
+
+    @action(methods=['post'], detail=False)
+    def register(self, request: HttpRequest, *args, **kwargs):
+        serializer: UserSerializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        average_sleep_time_hour, average_sleep_time_minute = map(
+            int, request.data['average_sleep_time'].split(":"))
+        bedtime_starts_at_hour, bedtime_starts_at_minute = map(
+            int, request.data['bedtime_starts_at'].split(":"))
+        serializer.save(
+            average_sleep_time=time(average_sleep_time_hour,
+                                    average_sleep_time_minute),
+            bedtime_starts_at=time(bedtime_starts_at_hour,
+                                   bedtime_starts_at_minute),
+        )
+        response_data = serializer.data
+
+        response_data['average_sleep_time'] = format_time(
+            response_data['average_sleep_time'])
+        response_data['bedtime_starts_at'] = format_time(
+            response_data['bedtime_starts_at']
+        )  # change time format from %H:%M:%S to %H:%M
+        return Response(response_data, status=status.HTTP_201_CREATED)
