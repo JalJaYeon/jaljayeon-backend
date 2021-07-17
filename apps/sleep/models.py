2551f1b32bd7e8c3579a188bc52b9ad11fa2d033
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-18 00:50:59 +0900

    feat(sleep): Sleep 모델 추가

--- apps/sleep/models.py
+++ apps/sleep/models.py
@@ -1,3 +1,14 @@
+from datetime import time
 from django.db import models
+from apps.common.models import BaseModel
+from apps.user.models import User
 
-# Create your models here.
+
+class Sleep(BaseModel):
+    owner: User = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    slept_date = models.DateField(auto_now_add=True)
+    slept_time: time = models.TimeField(null=False)
+    is_enough_sleep = models.BooleanField(null=False)
+    used_phone_30_mins_before_sleep = models.BooleanField(null=False)
+    tiredness_level = models.PositiveSmallIntegerField(
+        null=False)  # should be between 1 and 5
