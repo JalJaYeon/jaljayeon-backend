5058ac789602718134c88194f9d2ab313357556a
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-18 00:53:37 +0900

    feat(api): 라우터에 SleepView 등록

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from apps.user.views import UserView
+from apps.sleep.views import SleepView
 
 router = DefaultRouter(trailing_slash=False)
 router.register(r'users', UserView, basename='users')
+router.register(r'sleep', SleepView, basename='sleep')
 
 urlpatterns = [
     path('token', TokenObtainPairView.as_view()),
