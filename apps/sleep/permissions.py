f6680b0b1271d76a6f2186cb187904fb24852d7e
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-18 00:52:25 +0900

    feat(sleep): IsOwner 권한 추가

---
+++ apps/sleep/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+from apps.user.models import User
+
+
+class IsOwner(permissions.IsAuthenticated):
+    def has_object_permission(self, request, _, obj):
+        user: User = request.user
+        return obj.owner == user
