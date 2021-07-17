be0dac1784011d56c50fae41f151170e675ba279
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2021-07-18 04:28:19 +0900

    fix: 사용자의 패스워드가 UserManager을 통해 암호화가 되지 않는 오류 수정

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -1,10 +1,14 @@
+import typing
 from datetime import time
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from apps.user.validators import validate_hour_minute_format
 from apps.user.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        validators=(UniqueValidator(queryset=User.objects.all()), ), )
     password = serializers.CharField(write_only=True, required=True)
     average_sleep_time = serializers.CharField(
         min_length=5,
@@ -19,6 +23,18 @@ class UserSerializer(serializers.ModelSerializer):
 
     average_sleep_time: time = serializers.TimeField(format="%H:%M")
     bedtime_starts_at: time = serializers.TimeField(format="%H:%M")
+
+    def create(self, data: typing.Dict):
+        user = User.objects.create(
+            username=data["username"],
+            name=data["name"],
+            weight_kg=data["weight_kg"],
+            average_sleep_time=data["average_sleep_time"],
+            bedtime_starts_at=data["bedtime_starts_at"],
+        )
+        user.set_password(data['password'])
+        user.save()
+        return user
 
     class Meta:
         model = User
